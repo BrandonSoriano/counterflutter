ceef982a2c85ad25f70145339689f41246a0f824
brandon <narniabuena764@gmail.com>
2023-09-14 11:09:48 -0600

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutterproject/presentation/counter_function.dart';
+import 'package:flutterproject/presentation/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    theme:  ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.black45
+    ),
+    home: const CounterFuntionScreen() 
+    );
+  }
+
+
+ }
